5740ddbfc0ce3333794dfecdad4f07c131c07330
lvlashnin <lvlashnin@gmail.com>
2026-04-07 05:21:32 +0300

    chore: init React project with Vite and add currency constants

---
+++ src/constants/gameCurrency.js
@@ -0,0 +1,7 @@
+export const CURRENCY_TYPES = [
+  { value: "UAH", label: "₴ Гривня" },
+  { value: "USD", label: "$ Долар США" },
+  { value: "EUR", label: "€ Євро" },
+  { value: "PLN", label: "zł Злотий" },
+  { value: "GBP", label: "£ Фунт стерлінгів" },
+];
